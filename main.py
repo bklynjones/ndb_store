042ddef445efa663a6e842df2ab6fadeaff4073e
Brian Jones <bklynjones@mac.com>
2014-09-01 21:42:26 -0400

    upload functioning
    Need to create a redirect that stores a valid url for download.

--- main.py
+++ main.py
@@ -5,51 +5,71 @@ import webapp2
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import ndb
+import StringIO
 
 
-default_device_val = 'stanley'
-
-def device_key(device_name = default_device_val):
-    """constructs Datastore key for device uploading blob entity """
-    return ndb.Key('DeviceGroup', device_name)
 
 
 class BlobRecorder(ndb.Model):
-    file = ndb.BlobKeyProperty()
-    blob_key = blobstore.BlobReferenceProperty()
+
+    blob_file_key = ndb.BlobKeyProperty()
+    recordentrytime = ndb.DateTimeProperty(
+    auto_now_add=True)
+
+    @classmethod
+    def query_list_of_blobs(cls,number_of_entries_to_fetch=0):
+
+      output_buffer = StringIO.StringIO()
+      
+      ## Return a list of blob refence keys for given device
+      blobs_query = cls.query().order(-BlobRecorder.recordentrytime)
+
+      if number_of_entries_to_fetch == 0:
+          blob_key_records = blobs_query.fetch()
+      else: blobs_query = blobs_query.fetch(number_of_entries_to_fetch)
+
+      for blob_key_record in blob_key_records:
+        entry_time = blob_key_record.recordentrytime #.strftime("%a,%b,%d,%H,%M,%S")
+        
+        output_buffer.write('{"datetime" :"%s",'%entry_time)
+        output_buffer.write('{"blobfile" :"%s",'%blob_key_record.blob_file_key)
 
 class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    upload_url = blobstore.create_upload_url('/upload')
-    self.response.out.write('<html><body>')
-    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-        name="submit" value="Submit"> </form></body></html>""")
+    def get(self):
+      upload_url = blobstore.create_upload_url('/upload')
+      self.response.out.write('<html><body><form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+      self.response.out.write("""Upload File: <input type="file" name="file">
+        <br><input type="submit" name="submit" value="Submit"><br> 
+        </form></body></html>""")
+
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-  def post(self):
-    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
-    blob_info = upload_files[0]
+ 
+      
 
-    device_name = default_device_val
+    def post(self):
+      
+      upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+      blob_info = upload_files[0]
+  
+      b = BlobRecorder(blob_file_key = blob_info.key())
+      b_key = b.put()
 
-    b = BlobRecorder(parent = device_key(device_name),
-                    file = blob_info.key())
-    b_key = b.put()
 
-
-    self.redirect('/serve/%s' % blob_info.key())
-
+      self.response.out.write('/serve/%s' % blob_info.key())
+      self.redirect('/serve/%s' % blob_info.key())
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
   def get(self, resource):
     resource = str(urllib.unquote(resource))
     blob_info = blobstore.BlobInfo.get(resource)
     self.response.write(blob_info)
-    self.response.out.write('hello')
+    self.send_blob(blob_info)
 
 class RetrieveHandler(blobstore_handlers.BlobstoreDownloadHandler):
      def get(self):
+
+
         # resource = str(urllib.unquote(resource))
         # blob_info = blobstore.BlobInfo.get(resource)
         # self.send_blob(blob_info)
@@ -60,10 +80,14 @@ class RetrieveHandler(blobstore_handlers.BlobstoreDownloadHandler):
 
 class RetrieveRecordsHandler(webapp2.RequestHandler):
   def get(self):
-    records = ndb.Key('no_device_name',file)
 
-    for record in records :
-        self.response.write(record)
+    this = self
+    
+    self.response.headers['Access-Control-Allow-Origin'] = '*'
+    blob_reference_keys = BlobRecorder.query_list_of_blobs()
+    self.response.write(blob_reference_keys)
+    
+
 
 
 
